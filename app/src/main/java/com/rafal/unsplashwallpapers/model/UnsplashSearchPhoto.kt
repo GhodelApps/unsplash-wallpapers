a6c1213d2ae5634487505d88788e8a37ed439d55
Rafal <rafalswiech3@gmail.com>
2021-08-02 11:02:27 +0200

    Added Room library, added room Entities for SearchPhoto and User

--- app/src/main/java/com/rafal/unsplashwallpapers/model/UnsplashSearchPhoto.kt
+++ app/src/main/java/com/rafal/unsplashwallpapers/model/UnsplashSearchPhoto.kt
@@ -1,10 +1,15 @@
 package com.rafal.unsplashwallpapers.model
 
+import androidx.room.Embedded
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 
+
+@Entity(tableName = "search_photo")
 data class UnsplashSearchPhoto(
-    val id: String,
+    @PrimaryKey val id: String,
     val description: String?,
     val urls: UnsplashPhotoUrls,
     val likes: Int,
-    val user: UnsplashUser
+    @Embedded val user: UnsplashUser
 )
--- app/src/main/java/com/rafal/unsplashwallpapers/model/UnsplashUser.kt
+++ app/src/main/java/com/rafal/unsplashwallpapers/model/UnsplashUser.kt
@@ -1,11 +1,16 @@
 package com.rafal.unsplashwallpapers.model
 
+import androidx.room.Embedded
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 
+
+@Entity(tableName = "user")
 data class UnsplashUser(
-    val id: String,
+    @PrimaryKey val id: String,
     val name: String,
     val username: String,
-    val profile_image: ProfileImage,
+    @Embedded val profile_image: ProfileImage,
     val total_likes: Int,
     val downloads: Int,
     val total_photos: Int
